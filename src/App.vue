acf29f975069cc16c3b01e81ec2da1522b347c91
Yury Notiron <not.iron.yuriy@inbox.ru>
2025-02-14 16:55:32 +0300

    Подключение Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,43 @@
 <script setup lang="ts">
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from './firebase/config';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const getData = async () => {
+interface ISong {
+  artist: string;
+  title: string;
+  year: number;
+}
+
+const songs = ref<ISong[]>([]);
+
+const getData = async <T extends ISong>(): Promise<T[]> => {
   const querySnapshot = await getDocs(collection(db, 'songs'));
-  querySnapshot.forEach(doc => {
-    console.log(doc.id, doc.data());
-  });
+  return querySnapshot.docs.map(doc => doc.data() as T);
 };
 
-onMounted(async () => await getData());
+onMounted(async (): Promise<void> => {
+  songs.value = await getData();
+});
 </script>
 
-<template></template>
+<template>
+  <main>
+    <v-card class="mx-auto pa-2 text-center" max-width="400">
+      <v-list>
+        <v-list-subheader class="justify-center">MY MUSIC</v-list-subheader>
+
+        <v-list-item v-for="(item, i) in songs" :key="i" :value="item" color="primary" rounded="shaped">
+          <template v-slot:prepend>
+            <v-icon class="mdi mdi-play-circle-outline opacity-30"></v-icon>
+          </template>
+
+          <v-list-item-title v-text="item.title" class="text-left"></v-list-item-title>
+          <p class="text-left">{{ item.artist }}</p>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </main>
+</template>
 
 <style lang="scss"></style>
